ec85d855622375592965fe4f107d1a526763a423
Anatoliy <toxerius7@gmail.com>
2018-03-20 10:37:19 +0200

    End Project - its Work

--- Ado_Net_Providers_Demo/Ado_Net_Providers_Demo/Form1.Designer.cs
+++ Ado_Net_Providers_Demo/Ado_Net_Providers_Demo/Form1.Designer.cs
@@ -28,12 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnGetAllProvider = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnRequest = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 36);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(295, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // btnGetAllProvider
+            // 
+            this.btnGetAllProvider.Location = new System.Drawing.Point(642, 33);
+            this.btnGetAllProvider.Name = "btnGetAllProvider";
+            this.btnGetAllProvider.Size = new System.Drawing.Size(198, 23);
+            this.btnGetAllProvider.TabIndex = 1;
+            this.btnGetAllProvider.Text = "Get All Providers";
+            this.btnGetAllProvider.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 146);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(989, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Connection String";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 229);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(773, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 201);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Sql Request";
+            // 
+            // btnRequest
+            // 
+            this.btnRequest.Location = new System.Drawing.Point(803, 226);
+            this.btnRequest.Name = "btnRequest";
+            this.btnRequest.Size = new System.Drawing.Size(198, 23);
+            this.btnRequest.TabIndex = 6;
+            this.btnRequest.Text = "Execute Request";
+            this.btnRequest.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 274);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(986, 358);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1025, 644);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnRequest);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnGetAllProvider);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnGetAllProvider;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnRequest;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- Ado_Net_Providers_Demo/Ado_Net_Providers_Demo/Form1.cs
+++ Ado_Net_Providers_Demo/Ado_Net_Providers_Demo/Form1.cs
@@ -2,19 +2,104 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Common;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
 
 namespace Ado_Net_Providers_Demo
 {
     public partial class Form1 : Form
     {
+        DbConnection dbConnection = null;
+        DbProviderFactory dbProviderFactory = null;
+        string providerName = "";
         public Form1()
         {
             InitializeComponent();
+            btnRequest.Enabled = false;
+            btnGetAllProvider.Click += BtnGetAllProvider_Click;
+            comboBox1.SelectedIndexChanged += ComboBox1_SelectedIndexChanged;
+            btnRequest.Click += BtnRequest_Click;
+            textBox2.TextChanged += TextBox2_TextChanged;
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Length > 5)
+            {
+                btnRequest.Enabled = true;
+            }
+            else
+            {
+                btnRequest.Enabled = false;
+            }
+        }
+
+        private void BtnRequest_Click(object sender, EventArgs e)
+        {
+            dbConnection.ConnectionString = textBox1.Text;
+            DbDataAdapter dbDataAdapter = dbProviderFactory.CreateDataAdapter();
+            dbDataAdapter.SelectCommand = dbConnection.CreateCommand();
+            dbDataAdapter.SelectCommand.CommandText = textBox2.Text;
+            DataTable dataTable = new DataTable();
+            try
+            {
+               
+                dbDataAdapter.Fill(dataTable);
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = dataTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dbProviderFactory = DbProviderFactories.GetFactory(comboBox1.SelectedItem.ToString());
+            dbConnection = dbProviderFactory.CreateConnection();
+            providerName = GetConnectionStringProvider(comboBox1.SelectedItem.ToString());
+            textBox1.Text = providerName;
+        }
+
+        private string GetConnectionStringProvider(string providerName)
+        {
+            string returnValue = "";
+            ConnectionStringSettingsCollection settings = ConfigurationManager.ConnectionStrings;
+            if(settings != null)
+            {
+                foreach (ConnectionStringSettings item in settings)
+                {
+                    if(item.ProviderName == providerName)
+                    {
+                        returnValue = item.ConnectionString;
+                        break;
+                    }
+                    
+                }
+            }
+
+            return returnValue;
+
+        }
+
+
+
+        private void BtnGetAllProvider_Click(object sender, EventArgs e)
+        {
+            DataTable dataTable = DbProviderFactories.GetFactoryClasses();
+            dataGridView1.DataSource = dataTable;
+            comboBox1.Items.Clear();
+            foreach (DataRow row in dataTable.Rows)
+            {
+                comboBox1.Items.Add(row["InvariantName"]);
+            }
         }
     }
 }
