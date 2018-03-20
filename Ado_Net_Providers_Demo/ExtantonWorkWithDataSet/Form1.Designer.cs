61f879f161c8d685e281865ef1c3aa5c8340a245
Anatoliy <toxerius7@gmail.com>
2018-03-20 12:14:50 +0200

    End Work

--- Ado_Net_Providers_Demo/ExtantonWorkWithDataSet/Form1.Designer.cs
+++ Ado_Net_Providers_Demo/ExtantonWorkWithDataSet/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -70,11 +71,22 @@
             this.dataGridView1.Size = new System.Drawing.Size(783, 302);
             this.dataGridView1.TabIndex = 3;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(289, 93);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(439, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "create new table and insert data";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1023, 471);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
@@ -93,6 +105,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Ado_Net_Providers_Demo/ExtantonWorkWithDataSet/Form1.cs
+++ Ado_Net_Providers_Demo/ExtantonWorkWithDataSet/Form1.cs
@@ -42,5 +42,40 @@ namespace ExtantonWorkWithDataSet
             DataView dataView = dataViewManager.CreateDataView(dataSet.Tables[0]);
             dataGridView1.DataSource = dataView;
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            sqlConnection = new SqlConnection(connectionString);
+
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+            SqlTransaction sqlTransaction = null;
+            try
+            {
+                sqlConnection.Open();
+                sqlTransaction = sqlConnection.BeginTransaction();
+                sqlCommand.Transaction = sqlTransaction;
+
+                sqlCommand.CommandText = @"create table tmp3( id int not null identity primary key, desc1 varchar(100), price int);";
+                sqlCommand.ExecuteNonQuery();
+
+                sqlCommand.CommandText = @"insert into tmp3(desc1, price)values('some desc1 v1', 45)";
+                sqlCommand.ExecuteNonQuery();
+
+                sqlCommand.CommandText = @"insert into tmp3(desc1, price)values('some desc2 v2', 455)";
+                sqlCommand.ExecuteNonQuery();
+
+                sqlTransaction.Commit();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                sqlTransaction.Rollback();
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
     }
 }
